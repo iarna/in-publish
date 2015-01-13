37f171182b18315c7f2e6854c0a8508afeea89cf
Rebecca Turner <turner@mikomi.org>
2015-01-13 00:03:36 -0500

    Make these scripts actually executable

--- cli.js
+++ cli.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 "use strict"
 var inPublish = require("./index.js")
 process.exit(inPublish()? 0: 1)
--- not-cli.js
+++ not-cli.js
@@ -1,2 +1,4 @@
+#!/usr/bin/env node
+"use strict"
 var inPublish = require("./index.js")
 process.exit(inPublish()? 1: 0)
