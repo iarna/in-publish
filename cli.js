309ce88b21eb99e279cfae5a274dafd5ed772a4f
Rebecca Turner <turner@mikomi.org>
2015-01-12 22:19:23 -0500

    initial

---
+++ cli.js
@@ -0,0 +1,3 @@
+"use strict"
+var inPublish = require("./index.js")
+process.exit(inPublish()? 0: 1)
---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict"
+module.exports = function () {
+  try {
+    var npm_config_argv = JSON.parse(process.env["npm_config_argv"])
+  }
+  catch (e) {
+    return false
+  }
+
+  if (typeof npm_config_argv != "object") process.exit(1)
+  if (! npm_config_argv.cooked) process.exit(1)
+  if (! npm_config_argv.cooked instanceof Array) process.exit(1)
+
+  var V
+  while ((V = npm_config_argv.cooked.shift()) !== undefined) {
+    if (/^-/.test(V)) continue
+    if (V == "publish") return true
+    return false
+  }
+  return false
+}
